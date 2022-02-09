cdfc6fba22042647dd762711d9a64e2081ef6513
Uenderss <meajudabarto@gmail.com>
2022-02-09 16:07:35 -0300

    implementação do layout Uol menu em Css e funcao JavaScript

---
+++ jscript/script.js
@@ -0,0 +1,7 @@
+function abrirMenu(){
+    document.querySelector(".sobreposicao").style.display = "flex";
+}
+
+function clicaSobreposicao(){
+    document.querySelector(".sobreposicao").style.display = "none";
+}
