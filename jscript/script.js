ec1b3ce0c840edfed803cc57fb2e0db78e81e7f7
Uenderss <meajudabarto@gmail.com>
2022-02-11 13:42:25 -0300

    implementado a funcao de enviar mensagem

--- jscript/script.js
+++ jscript/script.js
@@ -1,3 +1,15 @@
+function abrirMenu() {
+  // document.classList.remove(".ocultar");
+  contatos();
+  document.querySelector(".sobreposicao").style.display = "flex";
+
+
+}
+
+function clicaSobreposicao() {
+  document.querySelector(".sobreposicao").style.display = "none";
+}
+
 let usuario;
 login();
 
@@ -24,17 +36,6 @@ function loginError(erro) {
 
 
 
-
-
-
-
-function abrirMenu() {
-  document.querySelector(".sobreposicao").style.display = "flex";
-}
-
-function clicaSobreposicao() {
-  document.querySelector(".sobreposicao").style.display = "none";
-}
 function listarMensagem() {
   const promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
   promessa.then(processarResposta);
@@ -76,15 +77,7 @@ function renderizarMensagens(mensagem) {
     const elementoQueQueroQueApareca = document.querySelector('ul').lastElementChild;
     elementoQueQueroQueApareca.scrollIntoView();
 
-    // setInterval(
-    // axios({
-    //   method: 'post',
-    //   url: 'https://mock-api.driven.com.br/api/v4/uol/status',
-    //   data: {
-    //     name: usuario.name,
-    //   }
-    // }),5000);
-    
+    setInterval(estouOnline(), 5000);
     // setTimeout(function () {
     //   window.location.reload(1);
     // }, 3000);
@@ -99,3 +92,75 @@ function tratarErro(erro) {
 
 //avisar que estou online
 
+function estouOnline() {
+  const online = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", usuario);
+}
+
+
+//enviar mensagem
+
+
+function enviarMensagem() {
+  let texto = document.querySelector("textarea").value;
+  if (texto) {
+    const mensagem = {
+      from: usuario.name,
+      to: 'Todos',
+      text: texto,
+      type: "message",
+    };
+    // console.log(mensagem);
+    const chat = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", mensagem)
+    chat.then(mensagemOK);
+    chat.catch(mensagemErro);
+
+  } else {
+    alert("Você não escreveu nenhuma mensagem");
+  }
+  document.querySelector("textarea").value = "";
+}
+
+function mensagemOK() {
+  listarMensagem();
+}
+
+function mensagemErro(erro) {
+  alert("Não foi possivel enviar a mensagem");
+  console.erro(erro);
+}
+
+
+
+function contatos() {
+  const participante = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+  // participante.then(listarParticipantes); 
+  participante.then(listarContatos);
+}
+
+function listarContatos(response) {
+  const pessoas = response.data;
+  renderizarContatos(pessoas);
+}
+
+function renderizarContatos(pessoas) {
+  for (let i = 0; i < pessoas.length; i++) {
+
+    let montarContatos = `
+      <div class="person">
+        <span><ion-icon name="person-circle"></ion-icon></span>
+        <div class="name">
+           <span>${pessoas[i].name}</span>
+           <span></span>
+        </div>
+      </div>
+      `;
+
+    document.querySelector(".contatos").innerHTML += montarContatos;
+
+    const elementoQueQueroQueApareca = document.querySelector('.contatos').lastElementChild;
+    elementoQueQueroQueApareca.scrollIntoView();
+
+    setInterval(estouOnline(), 5000);
+
+  }
+}
